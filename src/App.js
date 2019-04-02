94b43d6d2c8a4bff6a4c784e08812fd7f367d3bf
xavier-castro <xavier_castro@icloud.com>
2019-04-02 17:27:55 -0500

    Add Material

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Error from "./components/Error";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: "#000000"
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: "#0066ff",
+      main: "#0044ff",
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: "#ffcc00"
+    },
+    // error: will use the default color
+    typography: {
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"'
+      ].join(",")
+    }
+  }
+});
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <NavBar />
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route component={Error} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <div>
+            <NavBar />
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route component={Error} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,47 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
-const Home = () => {
-  return;
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing.unit * 3,
+    paddingBottom: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  heroUnit: {
+    backgroundColor: theme.palette.background.paper
+  },
+  heroContext: {
+    backgroundColor: theme.palette.secondary.main,
+    height: "400px",
+    margin: "0 auto",
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`
+  }
+});
+
+const Home = props => {
+  const { classes } = props;
+  return (
+    <React.Fragment>
+      <div className={classes.heroUnit}>
+        <div className={classes.heroContext}>
+          <Typography component="h1" variant="h2" align="center" gutterBottom>
+            Hero
+          </Typography>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
-export default Home;
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Home);
